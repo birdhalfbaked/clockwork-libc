f6f6f0be7a59e2c11250918ae318e7c4e331efcb
Clifford Richardson <cmrallen+birdhalfbaked@gmail.com>
2026-04-09 13:41:54 +0200

    update docs and fix some of the generated code to be actually useful

--- include/clockwork/conn.h
+++ include/clockwork/conn.h
@@ -9,13 +9,40 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/// @brief Invalid socket value
+/// @details This is used to represent an invalid socket. It is used to
+/// represent an invalid socket in the socket field of the tcp_conn_t struct.
+#define CLOCKWORK_INVALID_SOCKET ((uintptr_t)-1)
+
+/// @brief TCP connection structure
+/// @details This is used to represent a TCP connection. It is used to
+/// represent a TCP connection in the tcp_conn_t struct.
 typedef struct {
   uintptr_t socket;
 } tcp_conn_t;
 
+/// @brief Connect to a server
+/// @param address The address of the server to connect to
+/// @param port The port of the server to connect to
+/// @param conn The TCP connection structure to connect to
+/// @return The result of the connection
 int8_t connect_to_server(const char *address, uint16_t port, tcp_conn_t *conn);
+
+/// @brief Disconnect from a server
+/// @param conn The TCP connection structure to disconnect from
+/// @return The result of the disconnection
 int8_t disconnect_from_server(tcp_conn_t *conn);
+
+/// @brief Send a message to a server
+/// @param conn The TCP connection structure to send the message to
+/// @param message The message to send
+/// @return The result of the send
 int8_t send_message(tcp_conn_t *conn, const message_t *message);
+
+/// @brief Receive a message from a server
+/// @param conn The TCP connection structure to receive the message from
+/// @param out_message The message to receive
+/// @return The result of the receive
 int8_t receive_message(tcp_conn_t *conn, message_t *out_message);
 
 #endif
--- lib/platform/windows/conn.c
+++ lib/platform/windows/conn.c
@@ -1,61 +1,67 @@
 #include "clockwork/conn.h"
 #include "clockwork/debug.h"
 #include "clockwork/errors.h"
+
+#define WIN32_LEAN_AND_MEAN
+
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
-#define WIN32_LEAN_AND_MEAN
 static bool winsock_initialized = false;
 
 int8_t assert_winsock_initialized() {
-  int result = ERR_CONN_NO_ERROR;
+  int8_t result = ERR_CONN_NO_ERROR;
   if (!winsock_initialized) {
     WSADATA wsa_data;
-    int result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
-    if (result != 0) {
-      winsock_initialized = false;
+    int startup_result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    if (startup_result != 0) {
       // print out the error for now
-      DEBUG_PRINT("Failed to initialize Winsock: %d\n", result);
+      DEBUG_PRINT("Failed to initialize Winsock: %d\n", startup_result);
       result = ERR_CONN_CONNECTION_FAILED;
+    } else {
+      winsock_initialized = true;
     }
-    winsock_initialized = true;
   }
-  return ERR_CONN_NO_ERROR;
+  return result;
 }
 
 int8_t connect_to_server(const char *address, uint16_t port, tcp_conn_t *conn) {
   int8_t result = assert_winsock_initialized();
+  conn->socket = CLOCKWORK_INVALID_SOCKET;
   DEBUG_PRINT("Winsock initialized: %d\n", result);
-  SOCKADDR_IN addr;
-  SOCKET conn_socket;
-  DEBUG_PRINT("Creating socket\n");
-  conn_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (conn_socket == INVALID_SOCKET) {
-    DEBUG_PRINT("Failed to create socket: %d\n", WSAGetLastError());
+  SOCKADDR_IN addr = {0};
+  SOCKET conn_socket = INVALID_SOCKET;
+
+  DEBUG_PRINT("Converting address: %s\n", address);
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  int addr_result = inet_pton(AF_INET, address, &addr.sin_addr);
+  if (addr_result <= 0) {
+    DEBUG_PRINT("Failed to convert address: %s with result: %d", address,
+                addr_result);
     result = ERR_CONN_CONNECTION_FAILED;
   }
 
-  DEBUG_PRINT("Converting address: %s\n", address);
   if (result == ERR_CONN_NO_ERROR) {
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    int addr_result = inet_pton(AF_INET, address, &addr.sin_addr);
-    if (addr_result == 0) {
-      DEBUG_PRINT("Failed to convert address: %s", address);
-      WSACleanup();
+    DEBUG_PRINT("Creating socket\n");
+    conn_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (conn_socket == INVALID_SOCKET) {
+      DEBUG_PRINT("Failed to create socket: %d\n", WSAGetLastError());
       result = ERR_CONN_CONNECTION_FAILED;
     }
   }
-  DEBUG_PRINT("Address converted: %lu\n", addr.sin_addr.s_addr);
 
   DEBUG_PRINT("Connecting to server\n");
   if (result == ERR_CONN_NO_ERROR) {
     int connect_result = connect(conn_socket, (SOCKADDR *)&addr, sizeof(addr));
     DEBUG_PRINT("Connect result: %d\n", connect_result);
-    conn->socket = conn_socket;
-    if (connect_result == SOCKET_ERROR) {
-      DEBUG_PRINT("Failed to connect to server: %d\n", WSAGetLastError());
-      WSACleanup();
+    if (connect_result == 0) {
+      conn->socket = (uintptr_t)conn_socket;
+    } else {
+      closesocket(conn_socket);
+      conn->socket = CLOCKWORK_INVALID_SOCKET;
+      DEBUG_PRINT("Failed to connect to server: %d with error: %d\n",
+                  connect_result, WSAGetLastError());
       result = ERR_CONN_CONNECTION_FAILED;
     }
   }
@@ -63,7 +69,17 @@ int8_t connect_to_server(const char *address, uint16_t port, tcp_conn_t *conn) {
   return result;
 }
 
-int8_t disconnect_from_server(tcp_conn_t *conn) { return ERR_CONN_NO_ERROR; }
+int8_t disconnect_from_server(tcp_conn_t *conn) {
+  if (conn->socket != CLOCKWORK_INVALID_SOCKET) {
+    int close_result = closesocket((SOCKET)conn->socket);
+    if (close_result != 0) {
+      DEBUG_PRINT("Failed to close socket: %d\n", close_result);
+      return ERR_CONN_DISCONNECTION_FAILED;
+    }
+    conn->socket = CLOCKWORK_INVALID_SOCKET;
+  }
+  return ERR_CONN_NO_ERROR;
+}
 int8_t receive_message(tcp_conn_t *conn, message_t *out_message) {
   return ERR_CONN_NO_ERROR;
 }
